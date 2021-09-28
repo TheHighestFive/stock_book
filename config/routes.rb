557b3b6e7f1b86981eee0d4cf0bea3fcddfc4844
Spiky Mike <Mike973A@gmail.com>
2021-09-28 12:50:22 -0400

    generated users controller and my_portfolio route, view, and action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
